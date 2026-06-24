dc80d9388b1a1d617e2c8039f02743e11d2b7275
sonjaschnik <katharina.meder@student.uni-halle.de>
2026-06-24 15:21:51 +0200

    added endpoint to urls.py

--- snips/urls.py
+++ snips/urls.py
@@ -7,3 +7,10 @@ urlpatterns = [
     path('api/chart', views.api_chart_data, name='chart_data'),
     path('chart/', views.chart_page, name='chart_page'),
 ]
+
+from . import views
+
+urlpatterns = [
+    # ... existing paths ...
+    path("api/deribit/", views.deribit),
+]
